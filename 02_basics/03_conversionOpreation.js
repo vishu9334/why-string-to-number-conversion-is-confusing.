690e23971821672627026c702cb8dc84fc9878f8
vishu9334 <144935874+vishu9334@users.noreply.github.com>
2024-01-05 03:25:05 +0000

    Operation

---
+++ 02_basics/03_conversionOpreation.js
@@ -0,0 +1,28 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2**2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = "vishal"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" +2 );
+// console.log(1 +"2" );
+// console.log("1" +"2");
+// console.log("1"+2+2 );
+// console.log(1+2+"2" );
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gmCounter = 100
+gmCounter++
+++gmCounter
+console.log(gmCounter);
